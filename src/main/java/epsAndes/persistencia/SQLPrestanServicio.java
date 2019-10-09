856669924031435708ac7ce6dd5a5882125de3c5
jecanizarez <je.canizarez@uniandes.edu.co>
2019-10-09 10:33:32 -0500

    RCC1

--- src/main/java/epsAndes/persistencia/SQLPrestanServicio.java
+++ src/main/java/epsAndes/persistencia/SQLPrestanServicio.java
@@ -1,7 +1,11 @@
 package epsAndes.persistencia;
 
+import java.util.List;
+
 import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
+
+import epsAndes.negocio.ServiciosAfiliado;
 
 class SQLPrestanServicio {
 
@@ -19,5 +23,17 @@ class SQLPrestanServicio {
 		Query q = pm.newQuery(SQL, "INSERT INTO " + pp.darTablaPrestanServicio() + "(idIPS, idServicio) values (?, ?) ");
 		q.setParameters(idIPS, idServicio);
 		return (long) q.executeUnique();
+	}
+	
+	public long darCantidadDeServiciosPrestadorPorUnaIps(PersistenceManager pm, String fechaInicial, long IPS, String fechaFinal)
+	{
+		Query q = pm.newQuery(SQL, "SELECT COUNT(idTipoServicio) FROM "+ pp.darTablaServiciosAfiliado()
+				+ "WHERE IPS = ?"
+				+ " AND CAST(fechaAsistida AS date) >= CAST("+ fechaInicial+ " AS date)+ "
+				+ " AND CAST(fechaAsistida AS date) <= CAST("+fechaFinal  +" AS date)"
+			    + " GROUP BY IPS");
+		q.setParameters(IPS);
+		return (long) q.executeUnique();
+
 	}
 }
